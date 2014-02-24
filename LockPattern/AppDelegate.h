dd74526081b236bad04715dde3e8e8e6feeac9b5
sandeep <sandeepy2k1@gmail.com>
2014-02-24 12:06:19 +0530

    Initial Commit

---
+++ LockPattern/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LockPattern
+//
+//  Created by Amit Suneja on 24/02/14.
+//  Copyright (c) 2014 ids. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LockPattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LockPattern
+//
+//  Created by Amit Suneja on 24/02/14.
+//  Copyright (c) 2014 ids. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
